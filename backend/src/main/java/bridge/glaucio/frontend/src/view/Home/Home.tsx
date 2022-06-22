4886db990ea3fd731fef9f72b8a5eb7a063ddf90
Glaucio Martins <glaucio.lima@youxgroup.com.br>
2022-06-22 16:20:31 -0300

    redesing

--- backend/src/main/java/bridge/glaucio/frontend/src/view/Home/Home.tsx
+++ backend/src/main/java/bridge/glaucio/frontend/src/view/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useTarefasQuery, useDeleteTarefaMutation } from "../../graphql/hooks.generated";
-import { DataTable, Icon, Button } from 'bold-ui'
+import { DataTable, Icon, Button, Grid, Cell, HFlow, Heading } from 'bold-ui'
 import {  ModalAdicionarTarefa } from './components/ModalAdicionarTarefa'
 import { Tarefa } from "../../graphql/types.generated"
 
@@ -68,58 +68,72 @@ export function Home() {
 
     return (
         <>
-            {!todosDadosTarefaLoading && todosDadosTarefa?.tarefas && todosDadosTarefa?.tarefas?.length > 0 &&
-                <DataTable<Tarefa>
-                    rows={todosDadosTarefa.tarefas as Tarefa[]}
-                    sort={sort}
-                    onSortChange={setSort}
-                    loading={todosDadosTarefaLoading}
-                    columns={[
-                        {
-                            name: 'id',
-                            header: 'ID',
-                            sortable: true,
-                            render: item => item.id,
-                        },
-                        {
-                            name: 'responsavel',
-                            header: 'responsavel',
-                            render: item => item.responsavel,
-                        },
-                        {
-                            name: 'descricao',
-                            header: 'Descrição',
-                            render: item => item.descricao,
-                        },
-                        {
-                            name: 'edit',
-                            align: 'right',
-                            render: item => (
-                                <Button onClick={() => abrirModalEditar(item)} size='small' skin='ghost'>
-                                    <Icon icon='penFilled' />
-                                </Button>
-                            ),
-                        },
-                        {
-                            name: 'delete',
-                            align: 'right',
-                            render: item => (
-                                <Button onClick={() => excluirTarefa(item.id)} size='small' skin='ghost'>
-                                    <Icon icon='trashFilled' />
-                                </Button>
-                            ),
-                        },
-                    ]}
-                />
-            }
-            <ModalAdicionarTarefa
-                onSucess={refetchAll}
-                formState={formState}
-                setFormState={setFormState}
-                action={action}
-                isOpen={isOpen}
-                setIsOpen={handleModal} />
-
+            <Grid gap={1} style={{ marginTop: '0rem', marginBottom: '0.25rem' }} justifyContent="center">
+                <Cell xs={12}>
+                    <HFlow justifyContent='center'>
+                        <Heading level={1}>ToDo List</Heading>
+                    </HFlow>
+                </Cell>
+                <Cell xs={10}>
+                    <HFlow>
+                        {!todosDadosTarefaLoading && todosDadosTarefa?.tarefas && todosDadosTarefa?.tarefas?.length > 0 &&
+                            <DataTable<Tarefa>
+                                rows={todosDadosTarefa.tarefas as Tarefa[]}
+                                sort={sort}
+                                onSortChange={setSort}
+                                loading={todosDadosTarefaLoading}
+                                columns={[
+                                    {
+                                        name: 'id',
+                                        header: 'ID',
+                                        sortable: true,
+                                        render: item => item.id,
+                                    },
+                                    {
+                                        name: 'responsavel',
+                                        header: 'responsavel',
+                                        render: item => item.responsavel,
+                                    },
+                                    {
+                                        name: 'descricao',
+                                        header: 'Descrição',
+                                        render: item => item.descricao,
+                                    },
+                                    {
+                                        name: 'edit',
+                                        align: 'right',
+                                        render: item => (
+                                            <Button onClick={() => abrirModalEditar(item)} size='small' skin='ghost'>
+                                                <Icon icon='penFilled' />
+                                            </Button>
+                                        ),
+                                    },
+                                    {
+                                        name: 'delete',
+                                        align: 'right',
+                                        render: item => (
+                                            <Button onClick={() => excluirTarefa(item.id)} size='small' skin='ghost'>
+                                                <Icon icon='trashFilled' />
+                                            </Button>
+                                        ),
+                                    },
+                                ]}
+                            />
+                        }
+                    </HFlow>
+                </Cell>
+                <Cell xs={10}>
+                    <HFlow justifyContent="center">
+                        <ModalAdicionarTarefa
+                            onSucess={refetchAll}
+                            formState={formState}
+                            setFormState={setFormState}
+                            action={action}
+                            isOpen={isOpen}
+                            setIsOpen={handleModal} />
+                    </HFlow>
+                </Cell>
+            </Grid>
         </>
     );
 }
